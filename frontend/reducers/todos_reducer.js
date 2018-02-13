794d914a388208309c1df8683e991e3861a5c79b
Jeffrey Chuc <jeffreychuc@gmail.com>
2018-02-13 11:32:17 -0800

    added first set of reducers

--- frontend/reducers/todos_reducer.js
+++ frontend/reducers/todos_reducer.js
@@ -1,9 +1,16 @@
 import merge from 'lodash/merge';
+import { RECEIVE_TODO, RECIEVE_TODOS } from '../actions/todo_actions';
 
-export const todosReducer = (state = {}, action) => {
+const todosReducer = (state = {}, action) => {
   switch(action.type) {
-    
+    case RECEIVE_TODO:
+      return action.todos
+    case RECIEVE_TODOS:
+      // [action.todo.id] so that its interpolated
+      return (merge({}, state, {[action.todo.id]: action.todo}));
     default:
       return state;
   }
 };
+
+export default todosReducer;
--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import Root from './components/root';
-// import configureStore from './store/store';
+import configureStore from './store/store';
 // import style from '../stylesheets/application.scss';
 
 document.addEventListener("DOMContentLoaded", () => {
-	// let store = configureStore();
+	let store = configureStore();
 	// window.dispatch = store.dispatch;
-	// window.getState = store.getState;
+	window.getState = store.getState;
 	const root = document.getElementById("root");
   const test = <h1>Todos App</h1>;
   // ReactDOM.render(<Root store={store}/>, root);
