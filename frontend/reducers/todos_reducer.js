873510497afd6f1979809aa65af329673391a2e8
Jeffrey Chuc <jeffreychuc@gmail.com>
2018-02-13 11:46:38 -0800

    finish redux cycle

--- frontend/reducers/todos_reducer.js
+++ frontend/reducers/todos_reducer.js
@@ -1,7 +1,22 @@
 import merge from 'lodash/merge';
 import { RECEIVE_TODO, RECIEVE_TODOS } from '../actions/todo_actions';
 
-const todosReducer = (state = {}, action) => {
+const initialState = {
+  1: {
+    id: 1,
+    title: 'wash car',
+    body: 'with soap',
+    done: false
+  },
+  2: {
+    id: 2,
+    title: 'wash dog',
+    body: 'with shampoo',
+    done: true
+  },
+};
+
+const todosReducer = (state = initialState, action) => {
   switch(action.type) {
     case RECEIVE_TODO:
       return action.todos
--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import Root from './components/root';
 import configureStore from './store/store';
+import { receiveTodo } from './actions/todo_actions';
 // import style from '../stylesheets/application.scss';
 
 document.addEventListener("DOMContentLoaded", () => {
 	let store = configureStore();
-	// window.dispatch = store.dispatch;
+	window.dispatch = store.dispatch;
 	window.getState = store.getState;
+	window.receiveTodo = receiveTodo;
 	const root = document.getElementById("root");
   const test = <h1>Todos App</h1>;
   // ReactDOM.render(<Root store={store}/>, root);
